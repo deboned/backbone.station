5ea021e69730d8fef2366a4ecad3eed21124c654
moonwave99 <diego.caponera@gmail.com>
2015-04-14 16:59:17 +0200

    There is no commit like the first.

---
+++ README.md
@@ -0,0 +1,36 @@
+# backbone.station
+
+A mixin for making life with Backbone.Radio even easier.
+
+## Instructions
+
+In order not to fill initialisers with `Backbone.Radio.channel('app').comply( 'do:smth', this.action, this)` all over the place, you can now put all the configuration in two `complyTo` and `replyTo` hashes like this:
+
+    var MyView = Backbone.View.extend(_.extend({}, Backbone.Station, {
+      complyTo: {
+        channelName: {
+          'commandName'        : 'someHandler',
+          'anotherCommandName' : function(params){ ... }
+        },
+        anotherChannelName: { ... }
+      },
+      replyTo: {
+        channelName: {
+          'requestName' : ...
+        }
+      },
+      ...
+      'someHandler' : function(params){ ... }
+    }));
+    
+You just need to call the `_initChannels` method in the initialiser, like this:
+
+    initialize: function(options){
+      this._initChannels.call(this, 'channelName', 'anotherChannelName');
+    }
+    
+In order to remover listeners on view/object disposal, run `_unsubscribe`:
+
+    onDestroy: function(){
+      this._unsubcribe.call(this);
+    }
---
+++ backbone.station.js
@@ -0,0 +1,76 @@
+// Backbone.Station v0.0.1
+(function(root, factory) {
+  if (typeof define === 'function' && define.amd) {
+    define(['backbone', 'underscore'], function(Backbone, _) {
+      return factory(Backbone, _);
+    });
+  }
+  else if (typeof exports !== 'undefined') {
+    var Backbone = require('backbone');
+    var _ = require('underscore');
+    module.exports = factory(Backbone, _);
+  }
+  else {
+    factory(root.Backbone, root._);
+  }
+}(this, function(Backbone, _) {
+  'use strict';
+
+  var previousStation = Backbone.Station;
+  var Station = Backbone.Station = {};
+  
+  Station.VERSION = '0.0.1';
+
+  Station.noConflict = function () {
+    Backbone.Station = previousStation;
+    return this;
+  };
+
+  _.extend(Station, {
+    _walk: function(_handler){
+      var iterator = function(list, actionType){
+        _.forEach(list, function(actions, channelName){
+          _.forEach(actions, function(handler, actionName){
+            var targetHandler = function(){};
+            if(_.isFunction(handler)){
+              targetHandler = _.bind(handler, this);
+            }else if(_.isFunction(this[handler])){
+              targetHandler = _.bind(this[handler], this);
+            }
+            _handler.call(this, channelName, actionType, actionName, targetHandler);
+          }, this);
+        }, this);
+      };
+    
+      _.forEach({
+        comply    : this.complyTo,
+        reply     : this.replyTo
+      }, iterator, this);    
+    },
+    _initChannels: function(){
+      // Init Backbone.Radio channels - e.g. this._initChannels('app', 'ui')
+      this.channels = _.reduce(arguments, function(memo, channel){
+        memo[channel] = Backbone.Radio.channel(channel);
+        return memo;
+      }, {});
+    
+      this._walk(function(channelName, actionType, actionName, targetHandler){
+        this.channels[channelName][actionType](actionName, targetHandler);      
+      });
+    },
+    _unsubcribe: function(){
+      this._walk(function(channelName, actionType, actionName, targetHandler){
+        switch(actionType){
+          case 'comply':
+            this.channels[channelName].stopComplying(actionName, targetHandler, this);
+            break;
+          case 'reply':
+            this.channels[channelName].stopReplying(actionName, targetHandler, this);
+            break;
+        }
+      });
+    }
+  });
+
+  return Station;
+}));
